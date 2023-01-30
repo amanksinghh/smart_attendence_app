cbd1632c880440955954b6a9468e2eb7467a2444
Amansrivastava534 <Amansrivastava534@gmail.com>
2023-01-30 13:09:46 +0530

    request leaves flutter toast color to green

--- lib/pages/tab_cards/request_leave_cards.dart
+++ lib/pages/tab_cards/request_leave_cards.dart
@@ -141,7 +141,7 @@ class _RequestLeaveCardsState extends State<RequestLeaveCards> {
               toastLength: Toast.LENGTH_SHORT,
               gravity: ToastGravity.CENTER,
               timeInSecForIosWeb: 1,
-              backgroundColor: Colors.red,
+              backgroundColor: Colors.green,
               textColor: Colors.white,
               fontSize: 16.0);
           throw Exception('Failed to create leaves');
