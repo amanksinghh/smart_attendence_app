eed0856d177c4234cdcb520a40a29969324a97fe
Navin Panchal <navinp@zignuts.com>
2023-02-21 14:29:49 +0530

    tflite_adding

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:percent_indicator/circular_percent_indicator.dart';
